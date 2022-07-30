be75cf922e3459e9958827682fa5185c054e014e
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-07-30 12:07:09 +0200

    Move code from sdformat to gz-usd (#1)
    * Move code from sdformat to gz-usd
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ src/usd_parser/USD2SDF.hh
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef USD_PARSER_USD2IGNITION_USD_HH_
+#define USD_PARSER_USD2IGNITION_USD_HH_
+
+#include <string>
+
+#include <ignition/usd/config.hh>
+#include "ignition/usd/UsdError.hh"
+
+#include "sdf/Root.hh"
+
+namespace gz
+{
+// Inline bracket to help doxygen filtering.
+inline namespace IGNITION_USD_VERSION_NAMESPACE {
+  namespace usd
+  {
+    /// \brief USD to SDF converter
+    /// This class helps generate the SDF file
+    class USD2SDF
+    {
+      /// \brief constructor
+      public: USD2SDF() = default;
+
+      /// \brief convert USD file to a sdf::Root object
+      /// \param[in] _fileName string containing USD filename.
+      /// \param[in] _useGazeboPlugins Whether _root should have gazebo plugins
+      /// in it (true) or not (false)
+      /// \param[out] _root Root element to populate with the equivalent sdf
+      /// information from _fileName.
+      /// \return gz::usd::UsdErrors, which is a list of UsdError objects.
+      /// An empty list means no errors occurred when populating _root with
+      /// the contentsof _fileName
+      public: gz::usd::UsdErrors Read(
+        const std::string &_fileName,
+        bool _useGazeboPlugins,
+        sdf::Root &_root);
+    };
+  }
+}
+}
+
+#endif
---
+++ src/usd_parser/USDMaterial.hh
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef USD_PARSER_USDMATERIAL_HH_
+#define USD_PARSER_USDMATERIAL_HH_
+
+// TODO(ahcorde) this is to remove deprecated "warnings" in usd, these warnings
+// are reported using #pragma message so normal diagnostic flags cannot remove
+// them. This workaround requires this block to be used whenever usd is
+// included.
+#pragma push_macro ("__DEPRECATED")
+#undef __DEPRECATED
+#include <pxr/usd/usdGeom/gprim.h>
+#pragma pop_macro ("__DEPRECATED")
+
+#include "sdf/Material.hh"
+#include <ignition/usd/config.hh>
+#include "ignition/usd/UsdError.hh"
+
+namespace gz
+{
+  // Inline bracket to help doxygen filtering.
+  inline namespace IGNITION_USD_VERSION_NAMESPACE {
+  //
+  namespace usd
+  {
+    /// brief Parse the material in a USD prim to a sdf::Material
+    /// If the prim is a pxr::UsdGeomGprim, get the color values. Otherwise,
+    /// if the prim is a pxr::UsdShadeMaterial, get the texture values
+    /// \param[in] _prim USD prim where the material is extracted
+    /// \param[out] _material The sdf::Material representation of _prim's
+    /// material
+    /// \return gz::usd::UsdErrors, which is a vector of UsdError objects.
+    /// Each UsdErrorincludes an error code and message. An empty vector
+    /// indicates no error.
+    gz::usd::UsdErrors ParseMaterial(const pxr::UsdPrim &_prim,
+        sdf::Material &_material);
+}
+}
+}
+#endif
