3d98f82c1fff693018c069f902b10d2c1c3fe814
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-02-27 19:04:07 +0100

    Try to fix CI
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -32,6 +32,18 @@ then
 
   apt-get install libboost-all-dev p7zip-full -y
 
+  wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/2019_U6.tar.gz
+  tar -xvf 2019_U6.tar.gz
+  cd oneTBB-2019_U6/
+  make -j 2
+  mkdir -p /tmp/install_tbb/lib
+  mkdir -p /tmp/install_tbb/include
+  cp build/*_release/libtbb* /tmp/install_tbb/lib
+  cp -r include/serial /tmp/install_tbb/include/
+  cp -r include/tbb /tmp/install_tbb/include/
+
+  cd ..
+
   wget https://github.com/PixarAnimationStudios/USD/archive/refs/tags/v$USD_VERSION.zip
   unzip v$USD_VERSION.zip
   cd USD-$USD_VERSION
@@ -44,6 +56,7 @@ then
     -DPXR_ENABLE_PYTHON_SUPPORT=OFF \
     -DBUILD_SHARED_LIBS=ON  \
     -DTBB_USE_DEBUG_BUILD=OFF  \
+    -DTBB_ROOT=/tmp/install_tbb \
     -DPXR_BUILD_DOCUMENTATION=OFF  \
     -DPXR_BUILD_TESTS=OFF  \
     -DPXR_BUILD_EXAMPLES=OFF  \
