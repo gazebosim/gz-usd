b86c422822041367ea261cf0d35395beedc9df37
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-09-04 10:59:27 +0200

    Try to fix CI
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- .github/ci/before_cmake.sh
+++ .github/ci/before_cmake.sh
@@ -21,52 +21,11 @@ if [ $return_code -eq 0 ]
 then
   # compile USD from sources
   cd /tmp
-  mkdir usd_binaries
-  cd usd_binaries
-
-  apt-get install libboost-all-dev p7zip-full -y
-
-  wget https://github.com/oneapi-src/oneTBB/archive/refs/tags/2019_U6.tar.gz
-  tar -xvf 2019_U6.tar.gz
-  cd oneTBB-2019_U6/
-  make -j 2
-  mkdir -p /tmp/install_tbb/lib
-  mkdir -p /tmp/install_tbb/include
-  cp build/*_release/libtbb* /tmp/install_tbb/lib
-  cp -r include/serial /tmp/install_tbb/include/
-  cp -r include/tbb /tmp/install_tbb/include/
-
-  cd ..
 
   wget https://github.com/PixarAnimationStudios/USD/archive/refs/tags/v$USD_VERSION.zip
   unzip v$USD_VERSION.zip
   cd OpenUSD-$USD_VERSION
-  mkdir build
-  cd build
-
-  cmake -DCMAKE_INSTALL_PREFIX="/tmp/USD" -DCMAKE_PREFIX_PATH="/tmp/USD" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DPXR_PREFER_SAFETY_OVER_SPEED=ON \
-    -DPXR_ENABLE_PYTHON_SUPPORT=OFF \
-    -DBUILD_SHARED_LIBS=ON  \
-    -DTBB_USE_DEBUG_BUILD=OFF  \
-    -DTBB_ROOT=/tmp/install_tbb \
-    -DPXR_BUILD_DOCUMENTATION=OFF  \
-    -DPXR_BUILD_TESTS=OFF  \
-    -DPXR_BUILD_EXAMPLES=OFF  \
-    -DPXR_BUILD_TUTORIALS=OFF  \
-    -DPXR_BUILD_USD_TOOLS=OFF  \
-    -DPXR_BUILD_IMAGING=OFF  \
-    -DPXR_BUILD_USD_IMAGING=OFF  \
-    -DPXR_BUILD_USDVIEW=OFF  \
-    -DPXR_BUILD_ALEMBIC_PLUGIN=OFF  \
-    -DPXR_BUILD_DRACO_PLUGIN=OFF  \
-    -DPXR_ENABLE_MATERIALX_SUPPORT=OFF  \
-    -DBoost_INCLUDE_DIR=/usr/include  \
-    -DBoost_NO_BOOST_CMAKE=FALSE \
-    ..
-
-  make -j$(nproc) install
+  python3 build_scripts/build_usd.py --build-variant release --no-tests --no-examples --no-tutorials --no-docs --no-python /tmp/USD
 fi
 
 cd $BUILD_DIR
